686b55bc73e7023263fdb32b2c0cbf51806aef35
Matsu-Y74 <37435149+Matsu-Y74@users.noreply.github.com>
2020-02-14 05:36:49 +0900

    Viscosityの取り扱い
    Viscosityが大きければ滑らかだったのを小さければ滑らかになるように変更

--- Assets/Object/Slime/SlimeStickyNode.cs
+++ Assets/Object/Slime/SlimeStickyNode.cs
@@ -43,7 +43,7 @@ public class SlimeStickyNode : MonoBehaviour2D
 		z += (DefaultPosition.x * Parent.Right2D + DefaultPosition.y * Parent.Up2D - LocalPosition2D) ;
 		z += (PrevConnectTo.LocalPosition2D - LocalPosition2D) * ((PrevConnectTo.LocalPosition2D - LocalPosition2D).magnitude - Parent.NormalLength_NodeConnection);
 		z += (NextConnectTo.LocalPosition2D - LocalPosition2D) * ((NextConnectTo.LocalPosition2D - LocalPosition2D).magnitude - Parent.NormalLength_NodeConnection);
-
-		rigidbody2D.AddForce(Viscosity * z,ForceMode2D.Force);
+		
+		rigidbody2D.AddForce(z / Viscosity,ForceMode2D.Force);
 	}
 }
