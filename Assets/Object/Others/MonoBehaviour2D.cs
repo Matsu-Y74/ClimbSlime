ecd498d125e39421b0bdc3e8fa12b5d83acd3862
Matsu-Y74 <37435149+Matsu-Y74@users.noreply.github.com>
2020-02-14 01:19:25 +0900

    Right2D,Up2Dの追加
    MonoBehaviour2DにRight2D,Up2Dを追加

--- Assets/Object/Others/MonoBehaviour2D.cs
+++ Assets/Object/Others/MonoBehaviour2D.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class MonoBehaviour2D : MonoBehaviour
 {
 	public Vector2 Position2D{
-		get{ return new Vector2(transform.position.x,transform.position.y); }
+		get{ return new Vector2(transform.position.x,transform.position.y).normalized; }
 		set{ transform.position = new Vector3(value.x,value.y,0); }
+	}
+	public Vector2 Right2D{
+		get{ return new Vector2(transform.right.x,transform.right.y).normalized; }
+	}
+	public Vector2 Up2D{
+		get{ return new Vector2(transform.up.x,transform.up.y).normalized; }
 	}
 }
