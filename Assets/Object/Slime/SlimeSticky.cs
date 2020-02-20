48c2a060d78344093938ad5fc070afdc21a2317b
Matsu-Y74 <37435149+Matsu-Y74@users.noreply.github.com>
2020-02-21 06:25:43 +0900

    Aerialの除去
    Aerialのチップを除去

--- Assets/Object/Slime/SlimeSticky.cs
+++ Assets/Object/Slime/SlimeSticky.cs
@@ -3,9 +3,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(EdgeCollider2D))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
 public class SlimeSticky : MonoBehaviour2D
 {
+	class SlimeRendering{
+		SlimeSticky Parent{get;}
+		MeshFilter Filter{get;}
+		Vector3[] vertexs{get;}
+		int[] vertexorder{get;}
+		public SlimeRendering(SlimeSticky parent, MeshFilter filter){
+			Parent = parent;
+			Filter = filter;
+			vertexs = new Vector3[Parent.VertexCount + 2];
+			vertexorder = new int[3 * Parent.VertexCount];
+			Utility_Parallel.IndexedParallelAction(i => {
+				vertexorder[3 * i    ] = 0;
+				vertexorder[3 * i + 1] = i + 2;
+				vertexorder[3 * i + 2] = i + 1;
+			},Parent.VertexCount);
+		}
+		public void Update() {
+			Vector3 origin = Parent.transform.position;
+			vertexs[0] = origin;
+			Utility_Parallel.IndexedSingleAction(Parent.slimeStickyNode,(n,i)=>
+				vertexs[i + 1] = n.transform.position);
+			Utility_Parallel.IndexedParallelAction(i => vertexs[i] -= origin,vertexs.Length);
+
+			Mesh mesh = new Mesh();
+			mesh.vertices = vertexs;
+			mesh.triangles = vertexorder;
+			mesh.RecalculateNormals();
+
+			Filter.mesh = mesh;
+		}
+	}
+
 	[SerializeField]
     public GameObject prefab_SlimeStickyNode;
 
@@ -67,25 +100,23 @@ public class SlimeSticky : MonoBehaviour2D
 		}
 	}
 
-	EdgeCollider2D stickyHull;
-	Vector2[] points_hull;
 	public float NormalRadius{get;private set;}
 	public float NormalLength_NodeConnection{get;private set;}
 	protected SlimeStickyNode[] slimeStickyNode{get; private set;}
+	private SlimeRendering slimeRendering{get;set;}
 
 	public void Initialize(Slime parent,int vertexcount,float volume,float weight,float viscosity){
 		Parent = parent;
 		VertexCount = vertexcount;
 		Volume = volume;
 
-		stickyHull = GetComponent<EdgeCollider2D>();
-		points_hull = new Vector2[VertexCount + 1];
+		Vector2[] points_hull_Initial = new Vector2[VertexCount + 1];
 
-		points_hull = Utility_Parallel.IndexedParallelFunc(i => 
+		points_hull_Initial = Utility_Parallel.IndexedParallelFunc(i => 
 			NormalRadius * new Vector2(Mathf.Cos(2f * Mathf.PI * i / VertexCount),Mathf.Sin(2f * Mathf.PI * i/ VertexCount))
 		,VertexCount + 1);
 		
-		slimeStickyNode = Utility_Parallel.IndexedSingleFunc(points_hull,(n,i) => {
+		slimeStickyNode = Utility_Parallel.IndexedSingleFunc(points_hull_Initial,(n,i) => {
 			if(i != VertexCount){
 				GameObject obj = GameObject.Instantiate(prefab_SlimeStickyNode,Position2D + n,Quaternion.identity,transform);
 				obj.name = $"{gameObject.name}_SlimeStickyNode{i}";
@@ -94,11 +125,12 @@ public class SlimeSticky : MonoBehaviour2D
 			else{ return null; }
 		},VertexCount + 1);
 		slimeStickyNode[VertexCount] = slimeStickyNode[0];
-		stickyHull.points = points_hull;
 		
 		Utility_Parallel.IndexedSingleAction(slimeStickyNode,(n,i) =>
-		n.Initialize(i,this,points_hull[i], slimeStickyNode[(i - 1 + VertexCount) % VertexCount], slimeStickyNode[(i + 1) % VertexCount]),VertexCount);
+		n.Initialize(i,this,points_hull_Initial[i], slimeStickyNode[(i - 1 + VertexCount) % VertexCount], slimeStickyNode[(i + 1) % VertexCount]),VertexCount);
 		
+		slimeRendering = new SlimeRendering(this,GetComponent<MeshFilter>());
+
 		Weight = weight;
 		Viscosity = viscosity;
 	}
@@ -110,8 +142,13 @@ public class SlimeSticky : MonoBehaviour2D
 		Utility_Parallel.SingleAction(slimeStickyNode, n => gravityCenter += n.Position2D , VertexCount);
 		Position2D = gravityCenter / VertexCount;
 		Utility_Parallel.SingleAction(slimeStickyNode,newPos,(n,p) => n.Position2D = p, VertexCount);
+		Utility_Parallel.IndexedSingleAction(i => {
+			if(i > 0 && (slimeStickyNode[i].Position2D - Position2D).magnitude > NormalRadius * 1.5f)
+				slimeStickyNode[i].Position2D = slimeStickyNode[i - 1].Position2D;
+		}
+		,VertexCount);
 
-		stickyHull.points = Utility_Parallel.SingleFunc(slimeStickyNode,n => n.LocalPosition2D);
+		slimeRendering.Update();
 
 		meanvelocitylog.AddFirst(Velocity);
 		while(meanvelocitylog.Count > meanvelocitylog_max)meanvelocitylog.RemoveLast();
